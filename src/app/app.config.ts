690ba39e34d8fc6ae66cc353972e40c1aa925956
Iqbal Adel <IAdel@gmx.de>
2024-08-01 13:08:25 +0200

    added firebase service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"da-bubble-e728c","appId":"1:255571591771:web:030b649a0f5865b2bbf57e","storageBucket":"da-bubble-e728c.appspot.com","apiKey":"AIzaSyAIi9jGtrLP4f4qdry-askwdqBCeYOeo9M","authDomain":"da-bubble-e728c.firebaseapp.com","messagingSenderId":"255571591771"})), provideFirestore(() => getFirestore())]
 };
