88b02b89363f40c5400fac9d106cba56244cb09d
Iqbal Adel <IAdel@gmx.de>
2024-08-02 13:24:04 +0200

    added firebase ref functions

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData, onSnapshot, doc, updateDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData, onSnapshot, doc, updateDoc, getDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -9,4 +9,22 @@ export class FirebaseService {
   firestore: Firestore = inject(Firestore);
 
   constructor() { }
+
+
+  getUsersRef(){
+    return collection(this.firestore, 'users');
+  }
+  getChannelsRef(){
+    return collection(this.firestore, 'channels');
+  }
+  getChatsRef(){
+    return collection(this.firestore, 'chats');
+  }
+
+  getSingleDocRef(collID: string, docID: string){
+    return doc(collection(this.firestore, collID), docID)
+  }
+
+
+
 }
