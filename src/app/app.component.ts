49a89e510f9d4ad777a8027ace2566cf22c544ff
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2024-08-02 10:15:40 +0200

    route-to-sign-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatCardModule],
+  imports: [CommonModule, RouterOutlet, MatCardModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isVisible: boolean = true;
   title = 'daBubble';
+  constructor(private router: Router) { }
+  
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Überprüfe den aktuellen Router-Link und setze den Zustand entsprechend
+        if (this.router.url === '/sign-up') {  // Passe die Route hier an
+          this.isVisible = false;
+        } else {
+          this.isVisible = true;
+        }
+      }
+    });
+  }
 }
