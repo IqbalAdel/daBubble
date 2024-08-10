a50047942949ce98f26f2d78ff6908f6dbf83224
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2024-08-10 10:07:28 +0200

    login-section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,6 @@ import { CommonModule } from '@angular/common';
 import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import { DevspaceComponent } from "./devspace/devspace.component";
-<<<<<<< HEAD
-import { MainComponent } from './main/main.component';
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MatCardModule, RouterLink, DevspaceComponent, MainComponent],
-=======
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-root',
@@ -21,7 +14,6 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
     RouterLink, 
     DevspaceComponent,
   ],
->>>>>>> fa7127f689e3d4fb8ca657f85fa50054df8bbc4c
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { DevspaceComponent } from '../devspace/devspace.component';
-import { GroupChatComponent } from '../group-chat/group-chat.component';
-import { HeaderComponent } from './header/header.component';
-=======
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { DevspaceComponent } from '../devspace/devspace.component';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
->>>>>>> fa7127f689e3d4fb8ca657f85fa50054df8bbc4c
 
 @Component({
   selector: 'app-main',
   standalone: true,
-<<<<<<< HEAD
-  imports: [DevspaceComponent, GroupChatComponent,MainComponent,HeaderComponent],
-=======
   imports: [
     RouterLink,
     RouterOutlet,
@@ -25,7 +16,6 @@ import { MatIconButton } from '@angular/material/button';
     MatIcon,
     MatIconModule,
   ],
->>>>>>> fa7127f689e3d4fb8ca657f85fa50054df8bbc4c
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
