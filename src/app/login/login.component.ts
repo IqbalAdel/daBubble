1077f94f7b659d4708a4b971b687fce8daf6fad8
A-Marbach <113348931+A-Marbach@users.noreply.github.com>
2024-08-15 16:03:14 +0200

    login-firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-login',
   standalone: true,
